4f492466afa6c421310482fa43cb7ac00afbf586
ghisnatzy <ghisna002@gmail.com>
2025-06-15 18:02:43 +0700

    upload file

---
+++ restapi/game/tebaklogo.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = (req, res) => {
+  const filePath = path.join(__dirname, "./database/tebaklogo.json");
+  const data = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+  const random = data[Math.floor(Math.random() * data.length)];
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(random, null, 2));
+};
